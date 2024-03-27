a9e29efb3034b2b0dae2dad6d2fc5b28e33f2356
mjombanorman <mjomban@gmail.com>
2024-03-27 13:59:37 +0300

    Completed the views,models and urls for the app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+# models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    content = models.TextField()
+    # GenericForeignKey to associate comments with any model
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.content
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.contenttypes.models import ContentType
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'posts/post_form.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'posts/post_form.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'posts/post_confirm_delete.html'
+
+
+class PostDetailView(DetailView, FormMixin):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.content_type = ContentType.objects.get_for_model(
+                self.model)
+            comment.object_id = self.object.id
+            comment.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
